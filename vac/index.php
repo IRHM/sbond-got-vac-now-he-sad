7e7088d5d2cfcd411d636160cc969a9f4d54035d
Mr <37304121+sbondo1234@users.noreply.github.com>
2020-03-02 10:23:21 +0000

    revert putting $SITE_URL in replace of ul.sbond.co

--- vac/index.php
+++ vac/index.php
@@ -9,8 +9,8 @@
     <meta name="viewport" content="width=device-width,height=device-height,initial-scale=1.0">
     <title>sbondVac</title>
     <link rel="stylesheet" href="<?= $SITE_URL ?>/css/main.css">
-    <link rel="shortcut icon" href="<?= $SITE_URL ?>/i/ico/vac/logo.png">
-    <link rel="shortcut icon" href="<?= $SITE_URL ?>/i/ico/vac/logo.svg">
+    <link rel="shortcut icon" href="https://ul.sbond.co/i/ico/vac/logo.png">
+    <link rel="shortcut icon" href="https://ul.sbond.co/i/ico/vac/logo.svg">
   </head>
   <body>
     <nav>
